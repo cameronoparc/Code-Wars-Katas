f64597151b4631f219a50bf0d94833b6b4a17782
Cameron <43043652+cameronoparc@users.noreply.github.com>
2019-11-13 08:41:00 -0700

    Add files via upload

---
+++ Credit Card Mask.js
@@ -0,0 +1,12 @@
+// return masked string
+function maskify(cc){
+  var alt = "";
+  for (var index = 0; index < cc.length; ++index){
+    if (index <= cc.length - 5) {
+      alt = alt.concat((cc[index]).replace(cc[index], "#"));
+    } else {
+      alt = alt.concat(cc[index]);
+    }
+  }
+  return alt
+}
